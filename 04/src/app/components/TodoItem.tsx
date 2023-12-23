00eefd00536cce6311741b63a22b2b9a5156a63f
Dhanraj Singh <sdhanraj300@gmail.com>
2023-12-23 12:39:39 +0530

    Added some lines of code

--- 04/src/app/components/TodoItem.tsx
+++ 04/src/app/components/TodoItem.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client "
 import React from "react";
 type TodoItemProps = {
   id: string;
---
+++ next01/app/about/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Link from "next/link";
+const About = () => {
+  return (
+    <div>
+      <Link href="/">
+        <h1>Home</h1>
+      </Link>
+      <h1>About Page</h1>
+    </div>
+  );
+};
+
+export default About;
